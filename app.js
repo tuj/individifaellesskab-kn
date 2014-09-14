7fcb6b63530d3e309dddec2a9c13c14f831fa918
Troels Ugilt Jensen <tujensen@gmail.com>
2014-09-14 12:37:08 +0200

    First test version

--- app.js
+++ app.js
@@ -1,35 +1,73 @@
 var app = angular.module('app', ['ngAnimate']);
 
-app.controller('IndexController', ['$scope', '$interval', function ($scope, $interval) {
-  $scope.i = kulturnat.i;
-  $scope.we = kulturnat.we;
-  
-  $scope.iIndexes = [];
-  $scope.showI = function(i) {
-    return $scope.iIndexes.indexOf(i) >= 0;
+app.controller('IndexController', ['$scope', '$interval', '$timeout', function ($scope, $interval, $timeout) {
+  $scope.lines = [];
+  $scope.lines[0] = [];
+  $scope.lines[1] = [];
+  $scope.lineIndex = 1;
+
+  $scope.highligtingInterval = undefined;
+  $scope.highlightIndex = 0;
+
+  var startHighlighting = function() {
+    $scope.highlightIndex = 0;
+
+    $scope.highligtingInterval = $interval(function() {
+      $scope.highlightIndex = $scope.highlightIndex + 1;
+    }, 6000);
+  };
+
+  var stopHightlighting = function() {
+    $scope.highlightIndex = 10;
+    if (angular.isDefined($scope.highligtingInterval)) {
+      $interval.cancel($scope.highligtingInterval);
+      $scope.highligtingInterval = undefined;
+    }
+  };
+
+  var startShow = function() {
+    stopHightlighting();
+
+    var nextLineIndex = $scope.lineIndex;
+
+    $scope.lineIndex = ($scope.lineIndex + 1) % 2;
+
+    $scope.lines[nextLineIndex] = [];
+
+    // Update next lines.
+    while ($scope.lines[nextLineIndex].length < 5) {
+      var i = Math.floor((Math.random() * kulturnat.i.length));
+      
+      if ($scope.lines[nextLineIndex].indexOf(kulturnat.i[i]) < 0) {
+        $scope.lines[nextLineIndex].push(kulturnat.i[i]);
+      }
+    }
+
+    $timeout(startHighlighting, 500);
+  };
+
+  // Setup first array.
+  while ($scope.lines[0].length < 5) {
+    var i = Math.floor((Math.random() * kulturnat.i.length));
+    
+    if ($scope.lines[0].indexOf(kulturnat.i[i]) < 0) {
+      $scope.lines[0].push(kulturnat.i[i]);
+    }
   }
-  
-  $scope.weIndexes = [];
-  $scope.showWe = function(i) {
-    return $scope.weIndexes.indexOf(i) >= 0;
+  // Setup second array.
+  while ($scope.lines[1].length < 5) {
+    var i = Math.floor((Math.random() * kulturnat.i.length));
+    
+    if ($scope.lines[1].indexOf(kulturnat.i[i]) < 0) {
+      $scope.lines[1].push(kulturnat.i[i]);
+    }
   }
-  
+
+  // Start highlighting
+  $timeout(startHighlighting, 500);
+
+  // Start interval.
   $interval(function() {
-    $scope.iIndexes = [];
-    $scope.weIndexes = [];
-  
-	while ($scope.iIndexes.length < 5) {
-		var i = Math.floor((Math.random() * kulturnat.i.length));
-		if ($scope.iIndexes.indexOf(i) < 0) {
-			$scope.iIndexes.push(i);
-		}
-	}
-	while ($scope.weIndexes.length < 5) {
-		var i = Math.floor((Math.random() * kulturnat.we.length));
-		if ($scope.weIndexes.indexOf(i) < 0) {
-			$scope.weIndexes.push(i);
-		}
-	}
-	
-    }, 5000);
+    startShow();
+  }, 30500);
 }]);
